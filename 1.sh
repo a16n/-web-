6d74ea424555d9efbbf9d176302f139d9d17b7d0
a16n <2994016494@qq.com>
2020-08-07 12:38:10 +0800

    提交更新

---
+++ 1.sh
@@ -0,0 +1,6 @@
+#cd ./M4XCTF                                                      #切到git仓库
+git remote add origin https://github.com/a16n/M4xlmum 
+git add .
+git push -u origin master                                         #提交文件   
+git commit -m "提交更新"                                        #
+git pull                                                             #更新本地代码
